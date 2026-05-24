772ce50a520b24f30b43ab6d7fd48f7a5c2d0395
imflawlezz <egorart6@gmail.com>
2026-05-24 21:51:00 +0200

    Initial Commit

---
+++ purseful-ios/purseful_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  purseful_iosApp.swift
+//  purseful-ios
+//
+//  Created by Yahor Artsiomchyk on 24/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct purseful_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
